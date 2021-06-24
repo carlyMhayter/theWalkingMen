ca65f1fc7e39a89d426569450b52cf8657023aa7
Carly Hater <carlyhater@gmail.com>
2021-06-24 12:00:05 -0700

    changed border to half black

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "98cfb034e97959ed2096"; }
+/******/ 		__webpack_require__.h = function() { return "dba6543b686b55210416"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
