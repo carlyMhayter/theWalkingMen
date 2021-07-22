b8ff1bc1c8d279cfb80fa56cc7db755a2ae837fe
Carly Hater <carlyhater@gmail.com>
2021-07-22 13:25:31 -0700

    amended styling and corrected errors on testimonial page

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ const Home = () => (
   <div className="page-content">
     <div className="page-title">About </div>
     <div className="content">
-      {/* Band info Tile */}
+      {/* Band info Tile Main */}
       <div className="tile tile1 bio-tile">
         <div className="img-tile inner-tile" id="band-photo" />
         <div className="text-tile bio-text-tile inner-tile">
@@ -13,6 +13,12 @@ const Home = () => (
             making San Diego their home it seemed only right to put together
             something very special.
           </p>
+        </div>
+      </div>
+      {/* Band info Tile Minor */}
+      <div className="tile tile1 bio-tile">
+        <div className="text-tile bio-text-tile inner-tile">
+          <img src="/scroll.png" width="300px" height="30px" id="scroll-1" />
           <p className="text-tile-text-minor bio-text">
             Walking Man was born from two musicians great respect for one
             another and their longing to continue to perform in venues across
@@ -23,12 +29,13 @@ const Home = () => (
             living room with songs like the Secret of Life. A truly heartfelt
             and authentic interpretation of the music of James Taylor.
           </p>
+          <img src="/scroll.png" width="300px" height="30px" />
         </div>
       </div>
       {/* first member */}
       <div className="tile tile2 bio-tile">
         <div className="text-tile bio-text-tile inner-tile">
-          <p className="text-tile-text-emp bio-name">Tim Gowdin</p>
+          <p className="text-tile-text-emp bio-name">Tim Godwin</p>
           <p className="member-info"> Vocalist </p>
           <p className="text-tile-text-minor bio-text">
             Godwin has worked with such notable artists as Air Supply, Laura
@@ -77,18 +84,18 @@ const Home = () => (
           </p>
         </div>
       </div>
-    </div>
-    {/* fifth member*/}
-    <div className="tile tile2 bio-tile">
-      <div className="text-tile inner-tile bio-text-tile">
-        <p className="text-tile-text-emp bio-name">Kylea Sheils</p>
-        <p className="member-info"> Main and back-up vocals</p>
-        <p className="text-tile-text-minor bio-text">
-          Life-long vocalist adds her wonderful vocals to the sound of Walking
-          Man.
-        </p>
+      {/* fifth member*/}
+      <div className="tile tile2 bio-tile">
+        <div className="text-tile inner-tile bio-text-tile">
+          <p className="text-tile-text-emp bio-name">Kylea Sheils</p>
+          <p className="member-info"> Main and back-up vocals</p>
+          <p className="text-tile-text-minor bio-text">
+            Life-long vocalist adds her wonderful vocals to the sound of Walking
+            Man.
+          </p>
+        </div>
+        <div id="unknown" className="img-tile inner-til artist-img" />
       </div>
-      <div id="unknown" className="img-tile inner-til artist-img" />
     </div>
   </div>
 );
