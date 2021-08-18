13db45a85876b808fdb8910cb1153d73cc6cc0e2
Carly Hater <carlyhater@gmail.com>
2021-08-17 20:43:46 -0700

    fixed testimonial page

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,6 +2,30 @@ import { useCallback, useEffect, useState } from "react";
 import Link from "next/link";
 import React from "react";
 
+const Links = [
+  { linkName: "Home", linkLoc: "/" },
+  {
+    linkName: "About",
+    linkLoc: "/about",
+  },
+  {
+    linkName: "Video",
+    linkLoc: "/video",
+  },
+  {
+    linkName: "Songlist",
+    linkLoc: "/songlist",
+  },
+  {
+    linkName: "Testimonials",
+    linkLoc: "/testimonials",
+  },
+  {
+    linkName: "Contact",
+    linkLoc: "/contact",
+  },
+];
+
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -29,36 +53,13 @@ const NavBar = () => {
       <nav className="navbar">
         <div className="link-container navbar-links">
           <div id="longDisplay" className="text-links ">
-            <div className="navlink">
-              <Link href="/">
-                <a> Home </a>
-              </Link>
-            </div>
-            <div className="navlink">
-              <Link href="/about">
-                <a> About </a>
-              </Link>
-            </div>
-            <div className="navlink">
-              <Link href="/video">
-                <a> Video </a>
-              </Link>
-            </div>
-            <div className="navlink ">
-              <Link href="/songlist">
-                <a> Song List </a>
-              </Link>
-            </div>
-            <div className="navlink">
-              <Link href="/testimonials">
-                <a> Testimonials </a>
-              </Link>
-            </div>
-            <div className="navlink">
-              <Link href="/contact">
-                <a> Contact </a>
-              </Link>
-            </div>
+            {Links.map((link) => (
+              <div className="navlink">
+                <Link href={link.linkLoc}>
+                  <a> {link.linkName} </a>
+                </Link>
+              </div>
+            ))}
           </div>
           <div className="navlink img-link">
             <Link href="/">
@@ -79,36 +80,13 @@ const NavBar = () => {
             />
             {isOpen && (
               <div id="myDropdown" className="text-links dropdown-content">
-                <div className="navlink">
-                  <Link href="/">
-                    <a> Home </a>
-                  </Link>
-                </div>
-                <div className="navlink">
-                  <Link href="/about">
-                    <a> About </a>
-                  </Link>
-                </div>
-                <div className="navlink">
-                  <Link href="/video">
-                    <a> Video </a>
-                  </Link>
-                </div>
-                <div className="navlink ">
-                  <Link href="/songlist">
-                    <a> Song List </a>
-                  </Link>
-                </div>
-                <div className="navlink">
-                  <Link href="/testimonials">
-                    <a> Testimonials </a>
-                  </Link>
-                </div>
-                <div className="navlink">
-                  <Link href="/contact">
-                    <a> Contact </a>
-                  </Link>
-                </div>
+                {Links.map((link) => (
+                  <div className="navlink">
+                    <Link href={link.linkLoc}>
+                      <a> {link.linkName} </a>
+                    </Link>
+                  </div>
+                ))}
               </div>
             )}
           </div>
